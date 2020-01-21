cbcaf54b7c3ec7db3804095d35832e42a5881a82
zainab-hasnain <zainy.h19@gmail.com>
2020-01-22 01:01:30 +0500

    Zainab Hasnain php assignment1

---
+++ index.php
@@ -0,0 +1,32 @@
+
+
+<?php
+
+$marks = array(
+    "stdOne" => array(
+        "subOne" => 10,
+        "subTwo" => 20,
+        "subThree" => 30
+    ),
+
+    "stdTwo" => array(
+        "subOne" => 20,
+        "subTwo" => 30,
+        "subThree" => 40
+    ),
+
+    "stdThree" => array(
+        "subOne" => 10,
+        "subTwo" => 30,
+        "subThree" => 40
+    ),
+
+    "stdFour" => array(
+        "subOne" => 20,
+        "subTwo" => 30,
+        "subThree" => 40
+    ),
+);
+echo "Display Marks: <br/>";
+print_r($marks);
+?>
